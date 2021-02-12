1dfdd7e306ba05b6e62888e2db3230e121ee24ec
Rosano Coutinho <pub@xeari.com>
2021-02-12 11:05:34 -0300

    Flag include stub

--- rollup-config.js
+++ rollup-config.js
@@ -1,4 +1,5 @@
 export default require('OLSKRollupScaffold').OLSKRollupScaffoldScanStart(__dirname, {
+	OLSKRollupScanIncludeStubs: [__dirname + '/rollup-start.js'],
 	OLSKRollupPluginSwapTokens: Object.assign(require('OLSKUIAssets').OLSKUIAssetsSwapTokens(), {
 	}),
 });
